bbd106ff29aa03f7890cc49fb3f50a29583cacae
royhe <roy@poprx.ca>
2017-06-17 21:19:29 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  devtool: 'eval-source-map',
+
+  entry:  __dirname + "/app/main.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: "json"
+      }
+    ]
+  },
+
+  devServer: {
+    contentBase: "./public",//index.html
+    colors: true,
+    historyApiFallback: true,
+    inline: true
+  } 
+}
+
+// var config = require('./config.json');
+
+// module.exports = function() {
+//   var greet = document.createElement('div');
+//   greet.textContent = config.greetText;
+//   return greet;
+// };
